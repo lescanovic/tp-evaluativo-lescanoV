544fc6467aa6997528d431fe39765d3a148dc9ac
lescanovic <victorialescanoarco@gmail.com>
2024-07-02 08:34:23 -0300

    Trabajo en el diseño de mi inicio

--- src/app/modules/autentificacion/autentificacion.module.ts
+++ src/app/modules/autentificacion/autentificacion.module.ts
@@ -5,6 +5,12 @@ import { AutentificacionRoutingModule } from './autentificacion-routing.module';
 import { IniciosesionComponent } from './pages/iniciosesion/iniciosesion.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -13,7 +19,13 @@ import { RegistroComponent } from './pages/registro/registro.component';
   ],
   imports: [
     CommonModule,
-    AutentificacionRoutingModule
+    AutentificacionRoutingModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+    MatSelectModule
   ]
 })
 export class AutentificacionModule { }
--- src/app/modules/autentificacion/pages/registro/registro.component.ts
+++ src/app/modules/autentificacion/pages/registro/registro.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
+  hide = true ;
 
+  async registrar(){
+    const credenciales = {
+        email: this.usuarios.email,
+        password : this.usuarios.password
+    }
+    const res = await  
+  }
+ 
 }
